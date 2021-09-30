932af7a59dd638b339673c725a069be3fa679ffa
John <jospinel@microsoft.com>
2021-09-30 09:29:53 -0400

    init source

---
+++ ModernUIApp.Dockerfile
@@ -0,0 +1,25 @@
+FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /src
+COPY ["ModernUIApp/ModernUIApp.csproj", "ModernUIApp/"]
+RUN dotnet restore "ModernUIApp/ModernUIApp.csproj"
+COPY . .
+WORKDIR "/src/ModernUIApp"
+RUN dotnet build "ModernUIApp.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ModernUIApp.csproj" -c Release -o /app/publish --no-restore
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+
+ENV ApiURL http://modernapiapp/api/customers
+ENV ASPNETCORE_ENVIRONMENT Development
+ENV LaunchDarklyKey sdk-648f2d04-80a7-4892-b563-62db2dec3c5c
+
+ENTRYPOINT ["dotnet", "ModernUIApp.dll"]
---
+++ ModernUIApp/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModernUIApp.Services;
+using Microsoft.ApplicationInsights.Extensibility;
+
+
+namespace ModernUIApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddScoped<ICustomerDataService, CustomerDataService>();
+			services.AddScoped<IFeatureFlagService, FeatureFlagService>();
+            services.AddSingleton<ITelemetryInitializer, ModernTelemetryInitializer>();
+            Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions aiOptions = new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions();
+             
+            aiOptions.EnableAdaptiveSampling = true;
+            aiOptions.EnableDebugLogger = true;
+            aiOptions.EnableHeartbeat = true;
+            aiOptions.EnablePerformanceCounterCollectionModule = true;
+            aiOptions.EnableRequestTrackingTelemetryModule = true;
+            aiOptions.EnableQuickPulseMetricStream = true;
+             
+            services.AddApplicationInsightsTelemetry(aiOptions);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
